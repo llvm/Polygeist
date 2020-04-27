dd7bf5edb42e740f2d1137c1ca47f4d793c2aedf
lorenzo chelini <l.chelini@icloud.com>
2020-04-27 08:46:58 +0200

    [Pet-to-MLIR] Distinguish bit width in pet_expr_double
    PET distinguishes between two types of const expressions integer and
    floating-point, but it does not differentiate on the bit width, which is an
    implementation choice. When creating a floating-point expression, get the type
    of the LHS to distinguish between F32 and F64.

--- mlir/tools/mlir-pet/Test/Inputs/scalar.c
+++ mlir/tools/mlir-pet/Test/Inputs/scalar.c
@@ -1,12 +1,15 @@
 void print_memref_f32(float a);
 
 int main() {
-#pragma scop 
+#pragma scop
+ 
   int k;
   k = 23; 
   
   float i = 200.0;
   print_memref_f32(i);
+
+  double b = 5.0;
 
 #pragma endscop
   return 0;
