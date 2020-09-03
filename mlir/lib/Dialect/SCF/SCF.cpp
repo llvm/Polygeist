5b01f7aa475394511584e34feb08db1c10515e4a
Alex Zinenko <zinenko@google.com>
2020-09-03 19:07:28 +0200

    Add SCF barrier op
    This op collects Write side effects from above and below.

--- mlir/lib/Dialect/SCF/SCF.cpp
+++ mlir/lib/Dialect/SCF/SCF.cpp
@@ -10,6 +10,7 @@
 #include "mlir/Dialect/MemRef/IR/MemRef.h"
 #include "mlir/Dialect/StandardOps/IR/Ops.h"
 #include "mlir/IR/BlockAndValueMapping.h"
+#include "mlir/IR/Operation.h"
 #include "mlir/IR/PatternMatch.h"
 #include "mlir/Support/MathExtras.h"
 #include "mlir/Transforms/InliningUtils.h"
@@ -1278,6 +1279,39 @@ struct RemoveEmptyParallelLoops : public OpRewritePattern<ParallelOp> {
 void ParallelOp::getCanonicalizationPatterns(RewritePatternSet &results,
                                              MLIRContext *context) {
   results.add<CollapseSingleIterationLoops, RemoveEmptyParallelLoops>(context);
+}
+
+//===----------------------------------------------------------------------===//
+// BarrierOp
+//===----------------------------------------------------------------------===//
+void print(OpAsmPrinter &out, BarrierOp) {
+  out << BarrierOp::getOperationName();
+}
+
+LogicalResult verify(BarrierOp) { return success(); }
+
+ParseResult parseBarrierOp(OpAsmParser &, OperationState &) {
+  return success();
+}
+
+void BarrierOp::getEffects(
+    SmallVectorImpl<MemoryEffects::EffectInstance> &effects) {
+
+  // Collect Write effect instances from other operations in the block. Note
+  // that the implementation of getEffects erases all effect instances that have
+  // the type other than the template parameter, so if this ever needs to
+  // collect different effect types, the filtering will have to be done locally.
+  Operation *op = getOperation();
+  for (Operation *it = op->getPrevNode(); it != nullptr;
+       it = it->getPrevNode()) {
+    if (auto iface = dyn_cast<MemoryEffectOpInterface>(it))
+      iface.getEffects<MemoryEffects::Write>(effects);
+  }
+  for (Operation *it = op->getNextNode(); it != nullptr;
+       it = it->getNextNode()) {
+    if (auto iface = dyn_cast<MemoryEffectOpInterface>(it))
+      iface.getEffects<MemoryEffects::Write>(effects);
+  }
 }
 
 //===----------------------------------------------------------------------===//
