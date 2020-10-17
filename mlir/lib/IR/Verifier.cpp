6936093908c87d12ccdfc87126722b1a0dbca2b2
William S. Moses <gh@wsmoses.com>
2020-10-16 20:36:01 -0400

    Handle more of rodinia cuda

--- mlir/lib/IR/Verifier.cpp
+++ mlir/lib/IR/Verifier.cpp
@@ -136,9 +136,12 @@ LogicalResult OperationVerifier::verifyBlock(Block &block) {
   // Verify the non-terminator operations separately so that we can verify
   // they has no successors.
   for (auto &op : llvm::make_range(block.begin(), std::prev(block.end()))) {
-    if (op.getNumSuccessors() != 0)
+    if (op.getNumSuccessors() != 0) {
+      block.dump();
+      op.dump();
       return op.emitError(
           "operation with block successors must terminate its parent block");
+    }
 
     if (failed(verifyOperation(op)))
       return failure();
