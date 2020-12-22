4cdc6e9e03d7950e1eebfa10f67d37dc6674a6b0
lorenzo chelini <l.chelini@icloud.com>
2020-12-22 09:09:49 +0100

    Drop spurious assert in legalizeErasedResult
    Removing the assert fix: Transforms/test-legalize-erased-op-with-uses.mlir

--- mlir/lib/Transforms/Utils/DialectConversion.cpp
+++ mlir/lib/Transforms/Utils/DialectConversion.cpp
@@ -2276,7 +2276,6 @@ LogicalResult OperationConverter::legalizeErasedResult(
         << "found live user of result #" << result.getResultNumber() << ": "
         << *liveUserIt;
     op->getBlock()->dump();
-    assert(0 && "what");
     return failure();
   }
   return success();
