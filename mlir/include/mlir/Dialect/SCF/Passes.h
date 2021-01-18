36d7047fafdc519bea8151147abbc0307e0c90a8
lorenzo chelini <l.chelini@tue.nl>
2021-01-18 17:33:42 +0100

    plumbing

--- mlir/include/mlir/Dialect/SCF/Passes.h
+++ mlir/include/mlir/Dialect/SCF/Passes.h
@@ -35,6 +35,9 @@ std::unique_ptr<Pass> createParallelLoopSpecializationPass();
 std::unique_ptr<Pass>
 createParallelLoopTilingPass(llvm::ArrayRef<int64_t> tileSize = {});
 
+/// Convert scf.while to scf.for.
+std::unique_ptr<Pass> createWhileOpToForOpPass();
+
 //===----------------------------------------------------------------------===//
 // Registration
 //===----------------------------------------------------------------------===//
