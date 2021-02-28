798b10167edfcf81822e256c84249671c57764dc
William S. Moses <gh@wsmoses.com>
2021-02-28 15:36:42 -0500

    Additional analysis upgrades

--- mlir/tools/mlir-clang/Lib/clang-mlir.cc
+++ mlir/tools/mlir-clang/Lib/clang-mlir.cc
@@ -1360,7 +1360,7 @@ ValueWithOffsets MLIRScanner::VisitBinaryOperator(clang::BinaryOperator *BO) {
       if (bit->isSignedInteger())
         signedType = true;
     }
-    if (postTy != res.getType()) {
+    if (postTy != prevTy) {
       if (signedType) {
         res = builder.create<mlir::SignExtendIOp>(loc, res, postTy);
       } else {
