d84073085b55749d8d8e6eba0430e22ef0210e20
Vincent Zhao <vincentzhaorz@gmail.com>
2021-03-05 14:15:30 +0000

    Changed the scop.splittable value to an ID

--- tools/polymer/lib/Transforms/ScopStmtOpt.cc
+++ tools/polymer/lib/Transforms/ScopStmtOpt.cc
@@ -31,46 +31,67 @@ using namespace llvm;
 using namespace polymer;
 
 namespace {
+struct OpSplitByScratchpadPass
+    : public mlir::PassWrapper<OpSplitByScratchpadPass, OperationPass<FuncOp>> {
+  void runOnOperation() override {}
+};
+} // namespace
+
+static int annotateSplittable(FuncOp f, OpBuilder &b, int startId) {
+  int id = startId;
+
+  f.walk([&](mlir::AffineStoreOp storeOp) {
+    // Breadth first search to find the splittable operations.
+
+    // Stores operation and depth pairs.
+    std::queue<std::pair<Operation *, int>> worklist;
+    worklist.push(std::make_pair(storeOp, 0));
+
+    while (!worklist.empty()) {
+      std::pair<Operation *, int> front = worklist.front();
+      worklist.pop();
+
+      Operation *op = front.first;
+      int depth = front.second;
+
+      // Annotation.
+      if (depth > 1) {
+        op->setAttr("scop.splittable", b.getIndexAttr(id));
+        id++;
+      }
+
+      for (Value operand : op->getOperands()) {
+        Operation *defOp = operand.getDefiningOp();
+        // Filter out block arguments.
+        if (defOp == nullptr || operand.isa<BlockArgument>())
+          continue;
+        // Filter out operations out of the current region.
+        if (defOp->getParentRegion() != storeOp.getParentRegion())
+          continue;
+        // Filter out defining operations of specific types.
+        if (isa<mlir::AffineReadOpInterface, mlir::ConstantOp>(defOp))
+          continue;
+
+        worklist.push(std::make_pair(defOp, depth + 1));
+      }
+    }
+  });
+
+  return id - startId;
+}
+
+namespace {
 
 struct AnnotateSplittablePass
-    : public mlir::PassWrapper<AnnotateSplittablePass, OperationPass<FuncOp>> {
+    : public mlir::PassWrapper<AnnotateSplittablePass,
+                               OperationPass<ModuleOp>> {
   void runOnOperation() override {
-    FuncOp f = getOperation();
-    OpBuilder b(f.getContext());
+    ModuleOp m = getOperation();
+    OpBuilder b(m.getContext());
 
-    f.walk([&](mlir::AffineStoreOp storeOp) {
-      // Breadth first search to find the splittable operations.
-
-      // Stores operation and depth pairs.
-      std::queue<std::pair<Operation *, int>> worklist;
-      worklist.push(std::make_pair(storeOp, 0));
-
-      while (!worklist.empty()) {
-        std::pair<Operation *, int> front = worklist.front();
-        worklist.pop();
-
-        Operation *op = front.first;
-        int depth = front.second;
-
-        // Annotation.
-        if (depth > 1)
-          op->setAttr("scop.splittable", b.getUnitAttr());
-
-        for (Value operand : op->getOperands()) {
-          Operation *defOp = operand.getDefiningOp();
-          // Filter out block arguments.
-          if (defOp == nullptr || operand.isa<BlockArgument>())
-            continue;
-          // Filter out operations out of the current region.
-          if (defOp->getParentRegion() != storeOp.getParentRegion())
-            continue;
-          // Filter out defining operations of specific types.
-          if (isa<mlir::AffineReadOpInterface, mlir::ConstantOp>(defOp))
-            continue;
-
-          worklist.push(std::make_pair(defOp, depth + 1));
-        }
-      }
+    int numSplittable = 0;
+    m.walk([&](FuncOp f) {
+      numSplittable += annotateSplittable(f, b, numSplittable);
     });
   }
 };
