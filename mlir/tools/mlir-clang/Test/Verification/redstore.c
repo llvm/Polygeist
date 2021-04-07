06982a383f2c5e7d26dd592a58c0b57a82aafd4a
lorenzo chelini <l.chelini@icloud.com>
2021-04-07 12:46:02 +0000

    Rework reduction detection.
    Fixes:
    
    1. Remove detection of downstream loads, and emit a store operation
    directly to store the reduced dimension. The detection of downstream
    loads and the subsequence replacement of values were erroneous in case
    of multiple compatible loads.
    
    2. Make sure to have a single pair of load
    and store as a possible candidate.

--- mlir/tools/mlir-clang/Test/Verification/redstore.c
+++ mlir/tools/mlir-clang/Test/Verification/redstore.c
@@ -37,12 +37,13 @@ void caller(double* array) {
 // CHECK-NEXT:      affine.store %0, %arg0[0] : memref<?xf64>
 // CHECK-NEXT:      %1 = affine.load %arg0[0] : memref<?xf64>
 // CHECK-NEXT:      %2 = affine.for %arg3 = 0 to 10 iter_args(%arg4 = %1) -> (f64) {
-// CHECK-NEXT:        %4 = affine.load %arg1[%arg3] : memref<?xf64>
-// CHECK-NEXT:        %5 = addf %arg4, %4 : f64
-// CHECK-NEXT:        affine.yield %5 : f64
+// CHECK-NEXT:        %5 = affine.load %arg1[%arg3] : memref<?xf64>
+// CHECK-NEXT:        %6 = addf %arg4, %5 : f64
+// CHECK-NEXT:        affine.yield %6 : f64
 // CHECK-NEXT:      }
 // CHECK-NEXT:      affine.store %2, %arg0[0] : memref<?xf64>
-// CHECK-NEXT:      %3 = call @print(%2) : (f64) -> i32
+// CHECK-NEXT:      %3 = affine.load %arg0[0] : memref<?xf64>
+// CHECK-NEXT:      %4 = call @print(%3) : (f64) -> i32
 // CHECK-NEXT:    }
 // CHECK-NEXT:    return
 // CHECK-NEXT:  }
