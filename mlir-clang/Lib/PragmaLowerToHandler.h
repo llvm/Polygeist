3a2ae82dd70dc86fc721423ba3d818f85b28952f
Vincent Zhao <vincentzhaorz@gmail.com>
2021-05-14 21:28:26 +0100

    lower_to pragma handler

---
+++ mlir-clang/Lib/PragmaLowerToHandler.h
@@ -0,0 +1,17 @@
+#ifndef MLIR_TOOLS_MLIRCLANG_LIB_PRAGMALOWERTOHANDLER_H
+#define MLIR_TOOLS_MLIRCLANG_LIB_PRAGMALOWERTOHANDLER_H
+
+#include "clang/Lex/Preprocessor.h"
+#include "clang/Sema/Sema.h"
+
+#include "llvm/ADT/DenseMap.h"
+
+/// POD holds information processed from the lower_to pragma.
+struct LowerToInfo {
+  llvm::StringMap<std::string> SymbolTable;
+};
+
+void addPragmaLowerToHandlers(clang::Preprocessor &PP, clang::Sema &Actions,
+                              LowerToInfo &LTInfo);
+
+#endif
