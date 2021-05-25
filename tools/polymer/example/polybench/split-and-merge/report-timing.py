7e900653684cd798bfe7d1096580fe807069b1f2
Ruizhe Zhao <vincentzhaorz@gmail.com>
2021-05-25 16:32:14 +0000

    Add pluto results

--- tools/polymer/example/polybench/split-and-merge/report-timing.py
+++ tools/polymer/example/polybench/split-and-merge/report-timing.py
@@ -10,11 +10,14 @@ def main():
   parser.add_argument('work_dir', type=str)
   args = parser.parse_args()
 
+  results = []
+  pluto_results = []
   for fp in glob.glob(os.path.join(args.work_dir, '**', '*.log')):
 
     with open(fp, 'r') as f:
       lines = [line.strip() for line in f.readlines()]
       has_timing_report = any(('timing report' in line) for line in lines)
+      has_pluto = any(('[pluto] Timing statistics' in line) for line in lines)
 
       if has_timing_report:
         pos = next(i for i, line in enumerate(lines) if ('timing report' in line))
@@ -25,7 +28,19 @@ def main():
         pluto_time = float(next(x for x in lines if 'Pluto schedule elapsed' in x).split()[-1][:-1])
         name = '.'.join(os.path.basename(fp).split('.')[:4])
 
-        print('{name},{total_time},{pluto_pass_time},{pluto_time:.4f}'.format(name=name, total_time=total_time, pluto_pass_time=pluto_pass_time, pluto_time=pluto_time))
+        results.append('{name},{total_time},{pluto_pass_time},{pluto_time:.4f}'.format(name=name, total_time=total_time, pluto_pass_time=pluto_pass_time, pluto_time=pluto_time))
+      
+      elif has_pluto:
+        times = [str(float(y)) for y in next(x for x in lines if 'All times' in x).split()[-4:]]
+        pluto_results.append('.'.join(os.path.basename(fp).split('.')[:-1]) + ',' + ','.join(times))
+
+  
+  results.sort()
+  for line in results:
+    print(line)
+  pluto_results.sort()
+  for line in pluto_results:
+    print(line)
 
 if __name__ == '__main__':
   main()
