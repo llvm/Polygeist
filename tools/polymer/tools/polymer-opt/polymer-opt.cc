a4a4bda6711bbe12c0617695da141903410929f8
Ruizhe Zhao <kumasento@users.noreply.github.com>
2021-10-19 18:26:23 +0100

    [Reg2Mem] fixed issues lead to absent scratchpad (#102)
    * [Pass][ExtractScopStmt] internal index_cast
    
    * Added test cases
    
    * Removed unwanted tests

--- tools/polymer/tools/polymer-opt/polymer-opt.cc
+++ tools/polymer/tools/polymer-opt/polymer-opt.cc
@@ -35,45 +35,15 @@ using namespace llvm;
 using namespace mlir;
 using namespace polymer;
 
-static cl::opt<std::string>
-    inputFilename(cl::Positional, cl::desc("<input file>"), cl::init("-"));
-
-static cl::opt<std::string> outputFilename("o", cl::desc("Output filename"),
-                                           cl::value_desc("filename"),
-                                           cl::init("-"));
-
-static cl::opt<bool>
-    splitInputFile("split-input-file",
-                   cl::desc("Split the input file into pieces and process each "
-                            "chunk independently"),
-                   cl::init(false));
-
-static cl::opt<bool>
-    verifyDiagnostics("verify-diagnostics",
-                      cl::desc("Check that emitted diagnostics match "
-                               "expected-* lines on the corresponding line"),
-                      cl::init(false));
-
-static cl::opt<bool>
-    verifyPasses("verify-each",
-                 cl::desc("Run the verifier after each transformation pass"),
-                 cl::init(true));
-
-static cl::opt<bool> allowUnregisteredDialects(
-    "allow-unregistered-dialect",
-    cl::desc("Allow operation with no registered dialects"), cl::init(false));
-
 int main(int argc, char *argv[]) {
-  InitLLVM y(argc, argv);
-
   DialectRegistry registry;
 
   // Register MLIR stuff
   registry.insert<StandardOpsDialect>();
-  registry.insert<mlir::math::MathDialect>();
-  registry.insert<mlir::memref::MemRefDialect>();
   registry.insert<mlir::AffineDialect>();
   registry.insert<mlir::scf::SCFDialect>();
+  registry.insert<mlir::memref::MemRefDialect>();
+  registry.insert<mlir::math::MathDialect>();
   registry.insert<mlir::arith::ArithmeticDialect>();
   registry.insert<mlir::LLVM::LLVMDialect>();
 
@@ -83,12 +53,12 @@ int main(int argc, char *argv[]) {
   registerCSEPass();
   registerInlinerPass();
   // Register polymer specific passes.
-  polymer::registerPlutoTransformPass();
-  polymer::registerRegToMemPass();
-  polymer::registerExtractScopStmtPass();
-  polymer::registerScopStmtOptPasses();
-  polymer::registerLoopAnnotatePasses();
-  polymer::registerLoopExtractPasses();
+  registerPlutoTransformPass();
+  registerRegToMemPass();
+  registerExtractScopStmtPass();
+  registerScopStmtOptPasses();
+  registerLoopAnnotatePasses();
+  registerLoopExtractPasses();
 
   // Register any pass manager command line options.
   registerMLIRContextCLOptions();
@@ -97,26 +67,6 @@ int main(int argc, char *argv[]) {
   // Register printer command line options.
   registerAsmPrinterCLOptions();
 
-  PassPipelineCLParser passPipeline("", "Compiler passes to run");
-
-  // Parse pass names in main to ensure static initialization completed.
-  cl::ParseCommandLineOptions(argc, argv, "Polymer pass driver\n");
-
-  // Set up the input file.
-  std::string errorMessage;
-  auto file = openInputFile(inputFilename, &errorMessage);
-  if (!file) {
-    llvm::errs() << errorMessage << "\n";
-    return 1;
-  }
-
-  auto output = openOutputFile(outputFilename, &errorMessage);
-  if (!output) {
-    llvm::errs() << errorMessage << "\n";
-    exit(1);
-  }
-
-  return failed(MlirOptMain(output->os(), std::move(file), passPipeline,
-                            registry, splitInputFile, verifyDiagnostics,
-                            verifyPasses, allowUnregisteredDialects));
+  return failed(MlirOptMain(argc, argv, "Polymer optimizer driver", registry,
+                            /*preloadDialectsInContext=*/true));
 }
