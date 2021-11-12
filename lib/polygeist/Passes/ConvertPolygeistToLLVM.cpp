3e530fffe45db954b752a510696d2d1e4f4e32e9
Lorenzo Chelini <l.chelini@icloud.com>
2021-11-12 11:47:09 +0100

    fix clang-format

--- lib/polygeist/Passes/ConvertPolygeistToLLVM.cpp
+++ lib/polygeist/Passes/ConvertPolygeistToLLVM.cpp
@@ -157,14 +157,16 @@ void populatePolygeistToLLVMConversionPatterns(LLVMTypeConverter &converter,
   patterns.add<SubIndexOpLowering>(converter);
   patterns.add<Memref2PointerOpLowering>(converter);
   patterns.add<Pointer2MemrefOpLowering>(converter);
+  // clang-format on
 }
 
 namespace {
-struct ConvertPolygeistToLLVMPass : public ConvertPolygeistToLLVMBase<ConvertPolygeistToLLVMPass> {
+struct ConvertPolygeistToLLVMPass
+    : public ConvertPolygeistToLLVMBase<ConvertPolygeistToLLVMPass> {
   ConvertPolygeistToLLVMPass() = default;
   ConvertPolygeistToLLVMPass(bool useBarePtrCallConv, bool emitCWrappers,
-                   unsigned indexBitwidth, bool useAlignedAlloc,
-                   const llvm::DataLayout &dataLayout) {
+                             unsigned indexBitwidth, bool useAlignedAlloc,
+                             const llvm::DataLayout &dataLayout) {
     this->useBarePtrCallConv = useBarePtrCallConv;
     this->emitCWrappers = emitCWrappers;
     this->indexBitwidth = indexBitwidth;
@@ -184,8 +186,7 @@ struct ConvertPolygeistToLLVMPass : public ConvertPolygeistToLLVMBase<ConvertPol
 
     options.dataLayout = llvm::DataLayout(this->dataLayout);
 
-    LLVMTypeConverter converter(&getContext(), options,
-                                    &dataLayoutAnalysis);
+    LLVMTypeConverter converter(&getContext(), options, &dataLayoutAnalysis);
     RewritePatternSet patterns(&getContext());
     populatePolygeistToLLVMConversionPatterns(converter, patterns);
     populateMemRefToLLVMConversionPatterns(converter, patterns);
@@ -196,16 +197,17 @@ struct ConvertPolygeistToLLVMPass : public ConvertPolygeistToLLVMBase<ConvertPol
 
     LLVMConversionTarget target(getContext());
     target.addDynamicallyLegalOp<omp::ParallelOp, omp::WsLoopOp>(
-      [&](Operation *op) { return converter.isLegal(&op->getRegion(0)); });
+        [&](Operation *op) { return converter.isLegal(&op->getRegion(0)); });
     target.addLegalOp<omp::TerminatorOp, omp::TaskyieldOp, omp::FlushOp,
-                    omp::BarrierOp, omp::TaskwaitOp>();
+                      omp::BarrierOp, omp::TaskwaitOp>();
     if (failed(applyPartialConversion(m, target, std::move(patterns))))
       signalPassFailure();
   }
 };
 } // namespace
 
-std::unique_ptr<Pass> mlir::polygeist::createConvertPolygeistToLLVMPass(const LowerToLLVMOptions &options) {
+std::unique_ptr<Pass> mlir::polygeist::createConvertPolygeistToLLVMPass(
+    const LowerToLLVMOptions &options) {
   auto allocLowering = options.allocLowering;
   // There is no way to provide additional patterns for pass, so
   // AllocLowering::None will always fail.
