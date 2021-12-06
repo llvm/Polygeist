ac27529af3372e387f1b1485ece617c966d41fac
William S. Moses <gh@wsmoses.com>
2021-12-06 14:23:13 -0500

    Simplify string handling

--- mlir-clang/Test/Verification/virt2.cpp
+++ mlir-clang/Test/Verification/virt2.cpp
@@ -36,26 +36,26 @@ void make() {
 // CHECK-NEXT:     return
 // CHECK-NEXT:   }
 // CHECK:   func @_ZN3SubC1Eid(%arg0: !llvm.ptr<struct<(i8)>>, %arg1: i32, %arg2: f64) attributes {llvm.linkage = #llvm.linkage<linkonce_odr>} {
-// CHECK-NEXT:     %c0_i64 = arith.constant 0 : i64
+// CHECK-NEXT:     %c0_i32 = arith.constant 0 : i32
 // CHECK-NEXT:     %0 = llvm.bitcast %arg0 : !llvm.ptr<struct<(i8)>> to !llvm.ptr<struct<(i8)>>
 // CHECK-NEXT:     call @_ZN4RootC1Ei(%0, %arg1) : (!llvm.ptr<struct<(i8)>>, i32) -> ()
 // CHECK-NEXT:     call @_ZN5FRootC1Ev(%0) : (!llvm.ptr<struct<(i8)>>) -> ()
 // CHECK-NEXT:     %1 = llvm.mlir.addressof @str0 : !llvm.ptr<array<12 x i8>>
-// CHECK-NEXT:     %2 = llvm.getelementptr %1[%c0_i64, %c0_i64] : (!llvm.ptr<array<12 x i8>>, i64, i64) -> !llvm.ptr<i8>
+// CHECK-NEXT:     %2 = llvm.getelementptr %1[%c0_i32, %c0_i32] : (!llvm.ptr<array<12 x i8>>, i32, i32) -> !llvm.ptr<i8>
 // CHECK-NEXT:     call @_Z5printPc(%2) : (!llvm.ptr<i8>) -> ()
 // CHECK-NEXT:     return
 // CHECK-NEXT:   }
 // CHECK:   func @_ZN4RootC1Ei(%arg0: !llvm.ptr<struct<(i8)>>, %arg1: i32) attributes {llvm.linkage = #llvm.linkage<linkonce_odr>} {
-// CHECK-NEXT:     %c0_i64 = arith.constant 0 : i64
+// CHECK-NEXT:     %c0_i32 = arith.constant 0 : i32
 // CHECK-NEXT:     %0 = llvm.mlir.addressof @str1 : !llvm.ptr<array<13 x i8>>
-// CHECK-NEXT:     %1 = llvm.getelementptr %0[%c0_i64, %c0_i64] : (!llvm.ptr<array<13 x i8>>, i64, i64) -> !llvm.ptr<i8>
+// CHECK-NEXT:     %1 = llvm.getelementptr %0[%c0_i32, %c0_i32] : (!llvm.ptr<array<13 x i8>>, i32, i32) -> !llvm.ptr<i8>
 // CHECK-NEXT:     call @_Z5printPc(%1) : (!llvm.ptr<i8>) -> ()
 // CHECK-NEXT:     return
 // CHECK-NEXT:   }
 // CHECK:   func @_ZN5FRootC1Ev(%arg0: !llvm.ptr<struct<(i8)>>) attributes {llvm.linkage = #llvm.linkage<linkonce_odr>} {
-// CHECK-NEXT:     %c0_i64 = arith.constant 0 : i64
+// CHECK-NEXT:     %c0_i32 = arith.constant 0 : i32
 // CHECK-NEXT:     %0 = llvm.mlir.addressof @str2 : !llvm.ptr<array<14 x i8>>
-// CHECK-NEXT:     %1 = llvm.getelementptr %0[%c0_i64, %c0_i64] : (!llvm.ptr<array<14 x i8>>, i64, i64) -> !llvm.ptr<i8>
+// CHECK-NEXT:     %1 = llvm.getelementptr %0[%c0_i32, %c0_i32] : (!llvm.ptr<array<14 x i8>>, i32, i32) -> !llvm.ptr<i8>
 // CHECK-NEXT:     call @_Z5printPc(%1) : (!llvm.ptr<i8>) -> ()
 // CHECK-NEXT:     return
 // CHECK-NEXT:   }
