6be3389582024f49576263062fe61694d51f386a
William S. Moses <gh@wsmoses.com>
2022-01-19 00:16:32 -0500

    Fix affine raising

--- lib/polygeist/Passes/RaiseToAffine.cpp
+++ lib/polygeist/Passes/RaiseToAffine.cpp
@@ -118,13 +118,113 @@ struct ForOpRaising : public OpRewritePattern<scf::ForOp> {
   }
 };
 
+struct ParallelOpRaising : public OpRewritePattern<scf::ParallelOp> {
+  using OpRewritePattern<scf::ParallelOp>::OpRewritePattern;
+
+  // TODO: remove me or rename me.
+  bool isAffine(scf::ParallelOp loop) const {
+    for (auto step : loop.getStep())
+        if (!step.getDefiningOp<ConstantIndexOp>())
+            return false;
+    return true;
+  }
+
+  void canonicalizeLoopBounds(AffineParallelOp forOp) const {
+    SmallVector<Value, 4> lbOperands(forOp.getLowerBoundsOperands());
+    SmallVector<Value, 4> ubOperands(forOp.getUpperBoundsOperands());
+
+    auto lbMap = forOp.lowerBoundsMap();
+    auto ubMap = forOp.upperBoundsMap();
+    auto prevLbMap = lbMap;
+    auto prevUbMap = ubMap;
+
+    fully2ComposeAffineMapAndOperands(&lbMap, &lbOperands);
+    canonicalizeMapAndOperands(&lbMap, &lbOperands);
+
+    fully2ComposeAffineMapAndOperands(&ubMap, &ubOperands);
+    canonicalizeMapAndOperands(&ubMap, &ubOperands);
+
+    if (lbMap != prevLbMap)
+      forOp.setLowerBounds(lbOperands, lbMap);
+    if (ubMap != prevUbMap)
+      forOp.setUpperBounds(ubOperands, ubMap);
+  }
+
+  LogicalResult matchAndRewrite(scf::ParallelOp loop,
+                                PatternRewriter &rewriter) const final {
+      OpBuilder builder(loop);
+
+      if (loop.getResults().size()) return failure();
+
+      if (!llvm::all_of(loop.getLowerBound(), isValidIndex)) {
+        return failure();
+      }
+
+      if (!llvm::all_of(loop.getUpperBound(), isValidIndex)) {
+        return failure();
+      }
+
+      SmallVector<int64_t> steps;
+      for (auto step : loop.getStep())
+          if (auto cst = step.getDefiningOp<ConstantIndexOp>())
+              steps.push_back(cst.value());
+          else
+              return failure();
+
+      ArrayRef<AtomicRMWKind> reductions;
+      SmallVector<AffineMap> bounds;
+      for (size_t i=0; i<loop.getLowerBound().size(); i++)
+          bounds.push_back(AffineMap::get(/*dimCount=*/0, /*symbolCount=*/loop.getLowerBound().size(),
+                                                builder.getAffineSymbolExpr(i)));
+      AffineParallelOp affineLoop = rewriter.create<AffineParallelOp>(
+          loop.getLoc(), loop.getResultTypes(), reductions,
+          bounds, loop.getLowerBound(),
+          bounds, loop.getUpperBound(), 
+          steps);//, loop.getInitVals());
+
+      canonicalizeLoopBounds(affineLoop);
+
+      auto mergedYieldOp =
+          cast<scf::YieldOp>(loop.getRegion().front().getTerminator());
+
+      Block &newBlock = affineLoop.region().front();
+
+      // The terminator is added if the iterator args are not provided.
+      // see the ::build method.
+      if (affineLoop.getResults().size() == 0) {
+        auto affineYieldOp = newBlock.getTerminator();
+        rewriter.eraseOp(affineYieldOp);
+      }
+
+      rewriter.updateRootInPlace(loop, [&] {
+        affineLoop.region().front().getOperations().splice(
+            affineLoop.region().front().getOperations().begin(),
+            loop.getRegion().front().getOperations());
+
+        for (auto pair : llvm::zip(affineLoop.region().front().getArguments(),
+                                   loop.getRegion().front().getArguments())) {
+          std::get<1>(pair).replaceAllUsesWith(std::get<0>(pair));
+        }
+      });
+
+      rewriter.setInsertionPoint(mergedYieldOp);
+      rewriter.create<AffineYieldOp>(mergedYieldOp.getLoc(),
+                                     mergedYieldOp.getOperands());
+      rewriter.eraseOp(mergedYieldOp);
+
+      rewriter.replaceOp(loop, affineLoop.getResults());
+
+      return success();
+  }
+};
+
 void RaiseSCFToAffine::runOnFunction() {
   ConversionTarget target(getContext());
   target
       .addLegalDialect<AffineDialect, StandardOpsDialect, LLVM::LLVMDialect>();
 
   OwningRewritePatternList patterns(&getContext());
-  patterns.insert<ForOpRaising>(&getContext());
+  patterns.insert<ForOpRaising, ParallelOpRaising>(&getContext());
 
   if (failed(
           applyPartialConversion(getFunction(), target, std::move(patterns))))
