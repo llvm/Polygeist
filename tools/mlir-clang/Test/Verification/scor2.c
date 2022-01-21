ca7fa5cf0fbb5e05146de634869b0098e2919359
William S. Moses <gh@wsmoses.com>
2022-01-20 20:37:00 -0500

    Fix tests

--- tools/mlir-clang/Test/Verification/scor2.c
+++ tools/mlir-clang/Test/Verification/scor2.c
@@ -16,8 +16,10 @@ void kernel_correlation(int m, double corr[28])
 
 // CHECK:   func @kernel_correlation(%arg0: i32, %arg1: memref<?xf64>)
 // CHECK-NEXT:      %cst = arith.constant 0.000000e+00 : f64
+// CHECK-NEXT:      %c-1 = arith.constant -1 : index
 // CHECK-NEXT:      %0 = arith.index_cast %arg0 : i32 to index
-// CHECK-NEXT:      affine.for %arg2 = 0 to #map()[%0] {
+// CHECK-NEXT:      %1 = arith.addi %0, %c-1 : index
+// CHECK-NEXT:      affine.for %arg2 = 0 to %1 {
 // CHECK-NEXT:        affine.store %cst, %arg1[%arg2] : memref<?xf64>
 // CHECK-NEXT:      }
 // CHECK-NEXT:     return
