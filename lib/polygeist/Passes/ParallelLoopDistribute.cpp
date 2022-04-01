c0f4e698a92afc5ea4c4ecdc70e6af8751ccf99f
William S. Moses <gh@wsmoses.com>
2022-03-31 23:23:08 -0400

    Fix bug

--- lib/polygeist/Passes/ParallelLoopDistribute.cpp
+++ lib/polygeist/Passes/ParallelLoopDistribute.cpp
@@ -875,7 +875,7 @@ struct InterchangeForIfPFor : public OpRewritePattern<ParallelOpType> {
     }
 
     if (!arePreceedingOpsFullyRecomputable(
-            lastOp, /*isSingleExecution*/ isa<scf::IfOp, AffineIfOp>(op))) {
+            lastOp, /*isSingleExecution*/ isa<scf::IfOp, AffineIfOp>(lastOp))) {
       LLVM_DEBUG(DBGS() << "[interchange] found a nonrecomputable op\n");
       return failure();
     }
