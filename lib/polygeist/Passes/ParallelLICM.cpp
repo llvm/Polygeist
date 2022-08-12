a3fd21f633db02f56bf0cd8101618569e5cd5dce
William S. Moses <gh@wsmoses.com>
2022-08-12 18:14:12 -0400

    Add serial licm improvement

--- lib/polygeist/Passes/ParallelLICM.cpp
+++ lib/polygeist/Passes/ParallelLICM.cpp
@@ -29,8 +29,10 @@ struct ParallelLICM : public ParallelLICMBase<ParallelLICM> {
 } // namespace
 
 static bool canBeParallelHoisted(Operation *op, Operation *scope,
-                                 SmallPtrSetImpl<Operation *> &willBeMoved) {
+                                 SmallPtrSetImpl<Operation *> &willBeMoved,
+                                 bool includeAfter = false) {
   // Helper to check whether an operation is loop invariant wrt. SSA properties.
+  LLVM_DEBUG(llvm::dbgs() << "Checking for parallel hoist: " << *op << "\n");
   auto definedOutside = [&](Value value) {
     if (auto BA = value.dyn_cast<BlockArgument>())
       if (willBeMoved.count(BA.getOwner()->getParentOp()))
@@ -39,6 +41,8 @@ static bool canBeParallelHoisted(Operation *op, Operation *scope,
     if ((definingOp && !!willBeMoved.count(definingOp)) ||
         cast<LoopLikeOpInterface>(scope).isDefinedOutsideOfLoop(value))
       return true;
+    LLVM_DEBUG(llvm::dbgs()
+               << " - cannot hoist due to operand: " << value << "\n");
     return false;
   };
 
@@ -50,18 +54,20 @@ static bool canBeParallelHoisted(Operation *op, Operation *scope,
     SmallVector<MemoryEffects::EffectInstance, 1> effects;
     memEffect.getEffects(effects);
 
-    SmallVector<SideEffects::Resource *> readResources;
-    SmallVector<SideEffects::Resource *> writeResources;
-    SmallVector<SideEffects::Resource *> freeResources;
+    SmallVector<MemoryEffects::EffectInstance> readResources;
+    SmallVector<MemoryEffects::EffectInstance> writeResources;
+    SmallVector<MemoryEffects::EffectInstance> freeResources;
     for (auto effect : effects) {
-      if (isa<MemoryEffects::Allocate>(effect.getEffect()))
+      if (isa<MemoryEffects::Allocate>(effect.getEffect())) {
+        LLVM_DEBUG(llvm::dbgs() << " - cannot hoist due to allocation like\n");
         return false;
+      }
       if (isa<MemoryEffects::Read>(effect.getEffect()))
-        readResources.push_back(effect.getResource());
+        readResources.push_back(effect);
       if (isa<MemoryEffects::Write>(effect.getEffect()))
-        writeResources.push_back(effect.getResource());
+        writeResources.push_back(effect);
       if (isa<MemoryEffects::Free>(effect.getEffect()))
-        freeResources.push_back(effect.getResource());
+        freeResources.push_back(effect);
     }
 
     std::function<bool(Operation *)> conflicting = [&](Operation *b) {
@@ -81,48 +87,94 @@ static bool canBeParallelHoisted(Operation *op, Operation *scope,
       }
 
       auto memEffect = dyn_cast<MemoryEffectOpInterface>(b);
-      if (!memEffect)
+      if (!memEffect) {
+        LLVM_DEBUG(llvm::dbgs()
+                   << " - cannot hoist due to unknown memeffect conflict: "
+                   << *b << "\n");
         return true;
+      }
       for (auto res : readResources) {
         SmallVector<MemoryEffects::EffectInstance> effects;
-        memEffect.getEffectsOnResource(res, effects);
+        memEffect.getEffectsOnResource(res.getResource(), effects);
         for (auto effect : effects) {
-          if (isa<MemoryEffects::Allocate>(effect.getEffect()))
+          if (!mayAlias(effect, res))
+            continue;
+          if (isa<MemoryEffects::Allocate>(effect.getEffect())) {
+            LLVM_DEBUG(llvm::dbgs()
+                       << " - cannot hoist due to read->alloc conflict: " << *b
+                       << "\n");
             return true;
-          if (isa<MemoryEffects::Write>(effect.getEffect()))
+          }
+          if (isa<MemoryEffects::Write>(effect.getEffect())) {
+            LLVM_DEBUG(llvm::dbgs()
+                       << " - cannot hoist due to read->write conflict: " << *b
+                       << "\n");
             return true;
+          }
         }
       }
       for (auto res : writeResources) {
         SmallVector<MemoryEffects::EffectInstance> effects;
-        memEffect.getEffectsOnResource(res, effects);
+        memEffect.getEffectsOnResource(res.getResource(), effects);
         for (auto effect : effects) {
-          if (isa<MemoryEffects::Allocate>(effect.getEffect()))
+          if (!mayAlias(effect, res))
+            continue;
+          if (isa<MemoryEffects::Allocate>(effect.getEffect())) {
+            LLVM_DEBUG(llvm::dbgs()
+                       << " - cannot hoist due to write->alloc conflict: " << *b
+                       << "\n");
             return true;
-          if (isa<MemoryEffects::Read>(effect.getEffect()))
+          }
+          if (isa<MemoryEffects::Read>(effect.getEffect())) {
+            LLVM_DEBUG(llvm::dbgs()
+                       << " - cannot hoist due to write->read conflict: " << *b
+                       << "\n");
             return true;
+          }
         }
       }
       for (auto res : freeResources) {
         SmallVector<MemoryEffects::EffectInstance> effects;
-        memEffect.getEffectsOnResource(res, effects);
+        memEffect.getEffectsOnResource(res.getResource(), effects);
         for (auto effect : effects) {
-          if (isa<MemoryEffects::Allocate>(effect.getEffect()))
+          if (!mayAlias(effect, res))
+            continue;
+          if (isa<MemoryEffects::Allocate>(effect.getEffect())) {
+            LLVM_DEBUG(llvm::dbgs()
+                       << " - cannot hoist due to free->alloc conflict: " << *b
+                       << "\n");
             return true;
-          if (isa<MemoryEffects::Write>(effect.getEffect()))
+          }
+          if (isa<MemoryEffects::Write>(effect.getEffect())) {
+            LLVM_DEBUG(llvm::dbgs()
+                       << " - cannot hoist due to free->write conflict: " << *b
+                       << "\n");
             return true;
-          if (isa<MemoryEffects::Read>(effect.getEffect()))
+          }
+          if (isa<MemoryEffects::Read>(effect.getEffect())) {
+            LLVM_DEBUG(llvm::dbgs()
+                       << " - cannot hoist due to free->read conflict: " << *b
+                       << "\n");
             return true;
+          }
         }
       }
       return false;
     };
 
     std::function<bool(Operation *)> hasConflictBefore = [&](Operation *b) {
-      for (Operation *it = b->getPrevNode(); it != nullptr;
-           it = it->getPrevNode()) {
-        if (conflicting(it)) {
-          return true;
+      if (includeAfter) {
+        for (Operation &it : *b->getBlock()) {
+          if (conflicting(&it)) {
+            return true;
+          }
+        }
+      } else {
+        for (Operation *it = b->getPrevNode(); it != nullptr;
+             it = it->getPrevNode()) {
+          if (conflicting(it)) {
+            return true;
+          }
         }
       }
 
@@ -151,8 +203,11 @@ static bool canBeParallelHoisted(Operation *op, Operation *scope,
          freeResources.size()) &&
         hasConflictBefore(op))
       return false;
-  } else if (!op->hasTrait<OpTrait::HasRecursiveSideEffects>())
+  } else if (!op->hasTrait<OpTrait::HasRecursiveSideEffects>()) {
+    LLVM_DEBUG(llvm::dbgs()
+               << " - cannot hoist due to non memory/recursive side effects\n");
     return false;
+  }
 
   // Recurse into the regions for this op and check whether the contained ops
   // can be hoisted.
@@ -318,6 +373,136 @@ void moveParallelLoopInvariantCode(AffineParallelOp looplike) {
   LLVM_DEBUG(looplike.print(llvm::dbgs() << "\n\nModified loop:\n"));
 }
 
+void moveSerialLoopInvariantCode(scf::ForOp looplike) {
+  auto &loopBody = looplike.getLoopBody();
+
+  // We use two collections here as we need to preserve the order for insertion
+  // and this is easiest.
+  SmallPtrSet<Operation *, 8> willBeMovedSet;
+  SmallVector<Operation *, 8> opsToMove;
+
+  // Do not use walk here, as we do not want to go into nested regions and hoist
+  // operations from there. These regions might have semantics unknown to this
+  // rewriting. If the nested regions are loops, they will have been processed.
+  for (auto &block : loopBody) {
+    for (auto &op : block.without_terminator()) {
+      if (canBeParallelHoisted(&op, looplike, willBeMovedSet,
+                               /*checkAfter*/ true)) {
+        opsToMove.push_back(&op);
+        willBeMovedSet.insert(&op);
+      }
+    }
+  }
+
+  // For all instructions that we found to be invariant, move outside of the
+  // loop.
+  if (opsToMove.size()) {
+    OpBuilder b(looplike);
+    Value cond = b.create<arith::CmpIOp>(
+        looplike.getLoc(), CmpIPredicate::sle,
+        b.create<arith::AddIOp>(looplike.getLoc(), looplike.getLowerBound(),
+                                looplike.getStep()),
+        looplike.getUpperBound());
+    auto ifOp = b.create<scf::IfOp>(looplike.getLoc(), TypeRange(), cond);
+    looplike->moveBefore(ifOp.thenYield());
+  }
+  for (auto op : opsToMove)
+    looplike.moveOutOfLoop(op);
+  LLVM_DEBUG(looplike.print(llvm::dbgs() << "\n\nModified loop:\n"));
+}
+
+void moveSerialLoopInvariantCode(AffineForOp looplike) {
+  auto &loopBody = looplike.getLoopBody();
+
+  // We use two collections here as we need to preserve the order for insertion
+  // and this is easiest.
+  SmallPtrSet<Operation *, 8> willBeMovedSet;
+  SmallVector<Operation *, 8> opsToMove;
+
+  // Do not use walk here, as we do not want to go into nested regions and hoist
+  // operations from there. These regions might have semantics unknown to this
+  // rewriting. If the nested regions are loops, they will have been processed.
+  for (auto &block : loopBody) {
+    for (auto &op : block.without_terminator()) {
+      if (canBeParallelHoisted(&op, looplike, willBeMovedSet,
+                               /*checkAfter*/ true)) {
+        opsToMove.push_back(&op);
+        willBeMovedSet.insert(&op);
+      }
+    }
+  }
+
+  // For all instructions that we found to be invariant, move outside of the
+  // loop.
+  if (opsToMove.size()) {
+    OpBuilder b(looplike);
+
+    // TODO properly fill exprs and eqflags
+    SmallVector<AffineExpr, 2> exprs;
+    SmallVector<bool, 2> eqflags;
+
+    auto step = looplike.getStep();
+    for (auto ub : looplike.getUpperBoundMap().getResults()) {
+      SmallVector<AffineExpr, 4> symbols;
+      for (unsigned idx = 0; idx < looplike.getUpperBoundMap().getNumSymbols();
+           ++idx)
+        symbols.push_back(getAffineSymbolExpr(
+            idx + looplike.getLowerBoundMap().getNumSymbols(),
+            looplike.getContext()));
+
+      SmallVector<AffineExpr, 4> dims;
+      for (unsigned idx = 0; idx < looplike.getUpperBoundMap().getNumDims();
+           ++idx)
+        dims.push_back(
+            getAffineDimExpr(idx + looplike.getLowerBoundMap().getNumDims(),
+                             looplike.getContext()));
+
+      ub = ub.replaceDimsAndSymbols(dims, symbols);
+
+      for (auto lb : looplike.getLowerBoundMap().getResults()) {
+
+        // Bound is whether this expr >= 0, which since we want ub > lb, we
+        // rewrite as follows.
+        exprs.push_back(ub - lb - step);
+        eqflags.push_back(false);
+      }
+    }
+
+    SmallVector<Value> values;
+    auto lb_ops = looplike.getLowerBoundOperands();
+    auto ub_ops = looplike.getUpperBoundOperands();
+    for (unsigned idx = 0; idx < looplike.getLowerBoundMap().getNumDims();
+         ++idx) {
+      values.push_back(lb_ops[idx]);
+    }
+    for (unsigned idx = 0; idx < looplike.getUpperBoundMap().getNumDims();
+         ++idx) {
+      values.push_back(ub_ops[idx]);
+    }
+    for (unsigned idx = 0; idx < looplike.getLowerBoundMap().getNumSymbols();
+         ++idx) {
+      values.push_back(lb_ops[idx + looplike.getLowerBoundMap().getNumDims()]);
+    }
+    for (unsigned idx = 0; idx < looplike.getUpperBoundMap().getNumSymbols();
+         ++idx) {
+      values.push_back(ub_ops[idx + looplike.getUpperBoundMap().getNumDims()]);
+    }
+
+    auto iset = IntegerSet::get(
+        /*dim*/ looplike.getLowerBoundMap().getNumDims() +
+            looplike.getUpperBoundMap().getNumDims(),
+        /*symbols*/ looplike.getLowerBoundMap().getNumSymbols() +
+            looplike.getUpperBoundMap().getNumSymbols(),
+        exprs, eqflags);
+    auto ifOp = b.create<AffineIfOp>(looplike.getLoc(), TypeRange(), iset,
+                                     values, /*else*/ false);
+    looplike->moveBefore(ifOp.getThenBlock()->getTerminator());
+  }
+  for (auto op : opsToMove)
+    looplike.moveOutOfLoop(op);
+  LLVM_DEBUG(looplike.print(llvm::dbgs() << "\n\nModified loop:\n"));
+}
+
 void ParallelLICM::runOnOperation() {
   getOperation()->walk([&](LoopLikeOpInterface loopLike) {
     LLVM_DEBUG(loopLike.print(llvm::dbgs() << "\nOriginal loop:\n"));
@@ -326,6 +511,10 @@ void ParallelLICM::runOnOperation() {
       moveParallelLoopInvariantCode(par);
     } else if (auto par = dyn_cast<AffineParallelOp>((Operation *)loopLike)) {
       moveParallelLoopInvariantCode(par);
+    } else if (auto par = dyn_cast<scf::ForOp>((Operation *)loopLike)) {
+      moveSerialLoopInvariantCode(par);
+    } else if (auto par = dyn_cast<AffineForOp>((Operation *)loopLike)) {
+      moveSerialLoopInvariantCode(par);
     }
   });
 }
