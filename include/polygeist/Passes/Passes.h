2aab9448535bb8981d1462b5192fcc564a20636d
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2022-11-03 11:38:35 +0900

    Restructure passes

--- include/polygeist/Passes/Passes.h
+++ include/polygeist/Passes/Passes.h
@@ -29,7 +29,6 @@ createConvertPolygeistToLLVMPass(const LowerToLLVMOptions &options,
                                  bool useCStyleMemRef, bool onlyGpuModules);
 std::unique_ptr<Pass> createConvertPolygeistToLLVMPass();
 std::unique_ptr<Pass> createForBreakToWhilePass();
-std::unique_ptr<Pass> createRemoveDeviceFunctionsPass();
 std::unique_ptr<Pass> createConvertParallelToGPUPass1();
 std::unique_ptr<Pass> createConvertParallelToGPUPass2();
 std::unique_ptr<Pass> createGpuSerializeToCubinPass(
