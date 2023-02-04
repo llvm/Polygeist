575584f554a528b1aa7301d17db3d3631584ec60
Carl Guo <carlguo866@gmail.com>
2023-02-04 11:36:15 -0500

    reformat names; remove unimportant dependencies; add include guards

--- include/sql/SQLDialect.h
+++ include/sql/SQLDialect.h
@@ -1,4 +1,4 @@
-//===- BFVDialect.h - BFV dialect -----------------*- C++ -*-===//
+//===- SQLDialect.h - SQL dialect -----------------*- C++ -*-===//
 //
 // This file is licensed under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
