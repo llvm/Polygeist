3258a7206a63ef52308796e41563f6492f7fe342
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2023-05-16 15:01:08 +0900

    Compile RocmRuntimeWrappers

---
+++ lib/polygeist/ExecutionEngine/RocmRuntimeWrappers.cpp
@@ -0,0 +1,49 @@
+//===- PolygeistRocmRuntimeWrappers.cpp - MLIR ROCM API wrapper library ---===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Implements C wrappers around the ROCM library for easy linking in ORC jit.
+// Also adds some debugging helpers that are helpful when writing MLIR code to
+// run on GPUs.
+//
+//===----------------------------------------------------------------------===//
+
+#include "hip/hip_runtime.h"
+
+#ifdef _WIN32
+#define MLIR_HIP_WRAPPERS_EXPORT __declspec(dllexport) __attribute__((weak))
+#else
+#define MLIR_HIP_WRAPPERS_EXPORT __attribute__((weak))
+#endif // _WIN32
+
+#define HIP_REPORT_IF_ERROR(expr)                                              \
+  [](hipError_t result) -> hipError_t {                                        \
+    if (!result)                                                               \
+      return result;                                                           \
+    const char *name = hipGetErrorName(result);                                \
+    if (!name)                                                                 \
+      name = "<unknown>";                                                      \
+    fprintf(stderr, "'%s' failed with '%s'\n", #expr, name);                   \
+    return result;                                                             \
+  }(expr)
+
+extern "C" MLIR_HIP_WRAPPERS_EXPORT void *
+mgpurtMemAlloc(uint64_t sizeBytes, hipStream_t /*stream*/) {
+  void *ptr;
+  HIP_REPORT_IF_ERROR(hipMalloc(&ptr, sizeBytes));
+  return reinterpret_cast<void *>(ptr);
+}
+
+extern "C" void mgpurtMemcpyErr(void *dst, void *src, size_t sizeBytes) {
+  HIP_REPORT_IF_ERROR(hipMemcpy(dst, src, sizeBytes, hipMemcpyDefault));
+}
+
+extern "C" void mgpurtMemcpyAsyncErr(void *dst, void *src, size_t sizeBytes,
+                                     hipStream_t stream) {
+  HIP_REPORT_IF_ERROR(
+      hipMemcpyAsync(dst, src, sizeBytes, hipMemcpyDefault, stream));
+}
