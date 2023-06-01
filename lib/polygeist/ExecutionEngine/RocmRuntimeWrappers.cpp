cb1815ec0f1558b34fe4e9d600be6613f673d511
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2023-06-01 21:01:56 +0900

    Support cudaGetDeviceProperties hip translation

--- lib/polygeist/ExecutionEngine/RocmRuntimeWrappers.cpp
+++ lib/polygeist/ExecutionEngine/RocmRuntimeWrappers.cpp
@@ -130,3 +130,101 @@ extern "C" MLIR_HIP_WRAPPERS_EXPORT void
 __mgpurtUnregisterFatBinary(void **fatCubinHandle) {
   return __hipUnregisterFatBinary(fatCubinHandle);
 }
+
+#if POLYGEIST_ENABLE_CUDA
+
+#pragma push_macro("__forceinline__")
+#define __VECTOR_TYPES_H__
+#include <cuda_runtime_api.h>
+#undef __VECTOR_TYPES_H__
+#pragma pop_macro("__forceinline__")
+
+extern "C" MLIR_HIP_WRAPPERS_EXPORT int32_t
+mgpurtCudaGetDeviceProperties(struct cudaDeviceProp *cudaProp, int device) {
+  struct hipDeviceProp_t hipProp;
+  int err = ERR_HIP_REPORT_IF_ERROR(hipGetDeviceProperties(&hipProp, device));
+
+  // Reassign all corresponding fields to the hip props, the commented ones dont
+  // exist in hip one-for-one
+#define __polygeist_assign_field(f)                                            \
+  memcpy(&(cudaProp->f), &(hipProp.f), sizeof(cudaProp->f))
+  __polygeist_assign_field(name);
+  // __polygeist_assign_field(uuid);
+  __polygeist_assign_field(totalGlobalMem);
+  __polygeist_assign_field(sharedMemPerBlock);
+  __polygeist_assign_field(regsPerBlock);
+  __polygeist_assign_field(warpSize);
+  __polygeist_assign_field(memPitch);
+  __polygeist_assign_field(maxThreadsPerBlock);
+  __polygeist_assign_field(maxThreadsDim);
+  __polygeist_assign_field(maxGridSize);
+  __polygeist_assign_field(clockRate);
+  __polygeist_assign_field(totalConstMem);
+  __polygeist_assign_field(major);
+  __polygeist_assign_field(minor);
+  __polygeist_assign_field(textureAlignment);
+  __polygeist_assign_field(texturePitchAlignment);
+  // __polygeist_assign_field(deviceOverlap);
+  __polygeist_assign_field(multiProcessorCount);
+  __polygeist_assign_field(kernelExecTimeoutEnabled);
+  __polygeist_assign_field(integrated);
+  __polygeist_assign_field(canMapHostMemory);
+  __polygeist_assign_field(computeMode);
+  __polygeist_assign_field(maxTexture1D);
+  // __polygeist_assign_field(maxTexture1DMipmap);
+  __polygeist_assign_field(maxTexture1DLinear);
+  __polygeist_assign_field(maxTexture2D);
+  // __polygeist_assign_field(maxTexture2DMipmap);
+  // __polygeist_assign_field(maxTexture2DLinear);
+  // __polygeist_assign_field(maxTexture2DGather);
+  __polygeist_assign_field(maxTexture3D);
+  // __polygeist_assign_field(maxTexture3DAlt);
+  // __polygeist_assign_field(maxTextureCubemap);
+  // __polygeist_assign_field(maxTexture1DLayered);
+  // __polygeist_assign_field(maxTexture2DLayered);
+  // __polygeist_assign_field(maxTextureCubemapLayered);
+  // __polygeist_assign_field(maxSurface1D);
+  // __polygeist_assign_field(maxSurface2D);
+  // __polygeist_assign_field(maxSurface3D);
+  // __polygeist_assign_field(maxSurface1DLayered);
+  // __polygeist_assign_field(maxSurface2DLayered);
+  // __polygeist_assign_field(maxSurfaceCubemap);
+  // __polygeist_assign_field(maxSurfaceCubemapLayered);
+  // __polygeist_assign_field(surfaceAlignment);
+  __polygeist_assign_field(concurrentKernels);
+  __polygeist_assign_field(ECCEnabled);
+  __polygeist_assign_field(pciBusID);
+  __polygeist_assign_field(pciDeviceID);
+  __polygeist_assign_field(pciDomainID);
+  __polygeist_assign_field(tccDriver);
+  // __polygeist_assign_field(asyncEngineCount);
+  // __polygeist_assign_field(unifiedAddressing);
+  __polygeist_assign_field(memoryClockRate);
+  __polygeist_assign_field(memoryBusWidth);
+  __polygeist_assign_field(l2CacheSize);
+  // __polygeist_assign_field(persistingL2CacheMaxSize);
+  __polygeist_assign_field(maxThreadsPerMultiProcessor);
+  // __polygeist_assign_field(streamPrioritiesSupported);
+  // __polygeist_assign_field(globalL1CacheSupported);
+  // __polygeist_assign_field(localL1CacheSupported);
+  // __polygeist_assign_field(sharedMemPerMultiprocessor);
+  // __polygeist_assign_field(regsPerMultiprocessor);
+  __polygeist_assign_field(managedMemory);
+  __polygeist_assign_field(isMultiGpuBoard);
+  // __polygeist_assign_field(multiGpuBoardGroupID);
+  // __polygeist_assign_field(singleToDoublePrecisionPerfRatio);
+  __polygeist_assign_field(pageableMemoryAccess);
+  __polygeist_assign_field(concurrentManagedAccess);
+  // __polygeist_assign_field(computePreemptionSupported);
+  // __polygeist_assign_field(canUseHostPointerForRegisteredMem);
+  __polygeist_assign_field(cooperativeLaunch);
+  __polygeist_assign_field(cooperativeMultiDeviceLaunch);
+  __polygeist_assign_field(pageableMemoryAccessUsesHostPageTables);
+  __polygeist_assign_field(directManagedMemAccessFromHost);
+  // __polygeist_assign_field(accessPolicyMaxWindowSize);
+#undef __polygeist_assign_field
+
+  return err;
+}
+
+#endif
