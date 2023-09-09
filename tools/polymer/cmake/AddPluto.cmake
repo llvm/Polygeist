5a71dee02571e271f74a01f74506b29d962ea8a9
Emil VATAI <emil.vatai@gmail.com>
2023-09-09 11:19:21 +0900

    Add pluto & llvm10 to CMake (ExternalProject_Add)

--- tools/polymer/cmake/AddPluto.cmake
+++ tools/polymer/cmake/AddPluto.cmake
@@ -10,63 +10,9 @@ set(PLUTO_BINARY_DIR "${CMAKE_CURRENT_BINARY_DIR}/pluto")
 set(PLUTO_LIBCLANG_PREFIX "" CACHE STRING
     "The prefix to libclang used by Pluto (version < 10 required).")
 
-# If PLUTO_LIBCLANG_PREFIX is not set, we try to find a working version.
-# Note that if you set this prefix to a invalid path, then that path will be cached and 
-# the following code won't remedy that.
-if (NOT PLUTO_LIBCLANG_PREFIX)
-  message(STATUS "PLUTO_LIBCLANG_PREFIX not provided")
-
-  # If the provided CMAKE_CXX_COMPILER is clang, we will check its version and use its prefix if version is matched.
-  if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
-    if (${CMAKE_CXX_COMPILER_VERSION} VERSION_LESS 10)
-      execute_process(
-        COMMAND bash -c "which ${CMAKE_CXX_COMPILER}"  
-        OUTPUT_VARIABLE CLANG_ABSPATH
-      )
-      get_filename_component(CLANG_BINARY_DIR ${CLANG_ABSPATH} DIRECTORY)
-      get_filename_component(CLANG_PREFIX_DIR ${CLANG_BINARY_DIR} DIRECTORY)
-
-      message (STATUS "Provided CMAKE_CXX_COMPILER is clang of version less than 10 (${CMAKE_CXX_COMPILER_VERSION})") 
-      message (STATUS "Use its prefix for PLUTO_LIBCLANG_PREFIX: ${CLANG_PREFIX_DIR}")
-
-      set(PLUTO_LIBCLANG_PREFIX ${CLANG_PREFIX_DIR})
-    endif()
-  endif()
-
-endif()
-
 if (NOT PLUTO_LIBCLANG_PREFIX)
   set(PLUTO_LIBCLANG_PREFIX_CONFIG "")
-else()
-  # If a valid libclang is still not found, we try to search it on the system.
-  message(STATUS "PLUTO_LIBCLANG_PREFIX: ${PLUTO_LIBCLANG_PREFIX}")
-  set(PLUTO_LIBCLANG_PREFIX_CONFIG "--with-clang-prefix=${PLUTO_LIBCLANG_PREFIX}")
 endif()
-
-# Bootstrap Pluto
-if (NOT EXISTS "${PLUTO_SOURCE_DIR}/.git")
-  message(STATUS "Pluto not found at ${PLUTO_SOURCE_DIR}, downloading ...")
-  execute_process(COMMAND     ${POLYMER_SOURCE_DIR}/scripts/update-pluto.sh
-                  OUTPUT_FILE ${CMAKE_CURRENT_BINARY_DIR}/update-pluto.log)
-endif()
-
-# Get Pluto git commit.
-execute_process(COMMAND git rev-parse HEAD
-                OUTPUT_VARIABLE PLUTO_GIT_HASH
-                OUTPUT_STRIP_TRAILING_WHITESPACE
-                WORKING_DIRECTORY ${PLUTO_SOURCE_DIR})
-message(STATUS "Pluto git hash: ${PLUTO_GIT_HASH}")
-
-# Get all Pluto submodule git status 
-execute_process(COMMAND git submodule status --recursive
-                OUTPUT_VARIABLE PLUTO_SUBMODULE_GIT_STATUS
-                OUTPUT_STRIP_TRAILING_WHITESPACE
-                WORKING_DIRECTORY ${PLUTO_SOURCE_DIR})
-STRING(REGEX REPLACE "\n" ";" PLUTO_SUBMODULE_GIT_STATUS ${PLUTO_SUBMODULE_GIT_STATUS})
-foreach (submodule ${PLUTO_SUBMODULE_GIT_STATUS})
-  STRING(STRIP ${submodule} submodule)
-  message(STATUS "${submodule}")
-endforeach()
 
 # Pluto configuration shell script.
 string(CONCAT PLUTO_CONFIGURE_SHELL_SCRIPT
@@ -77,15 +23,32 @@ set(PLUTO_CONFIGURE_COMMAND "${CMAKE_CURRENT_BINARY_DIR}/configure-pluto.sh")
 file(GENERATE OUTPUT ${PLUTO_CONFIGURE_COMMAND}
      CONTENT ${PLUTO_CONFIGURE_SHELL_SCRIPT})
 
+set(PLUTO_LLVM_PREFIX "${CMAKE_CURRENT_BINARY_DIR}/llvm10")
 ExternalProject_Add(
-  pluto 
-  PREFIX ${PLUTO_BINARY_DIR}
-  SOURCE_DIR ${PLUTO_SOURCE_DIR}
-  CONFIGURE_COMMAND ${CMAKE_COMMAND} -E env bash ${PLUTO_CONFIGURE_COMMAND}
-  BUILD_COMMAND make -j 4
+  llvm10
+  GIT_REPOSITORY https://github.com/llvm/llvm-project.git
+  GIT_TAG release/10.x
+  CMAKE_ARGS -DLLVM_ENABLE_PROJECTS=clang -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=X86 -DCMAKE_INSTALL_PREFIX=${PLUTO_LLVM_PREFIX}/llvm10-install
+  PREFIX llvm10
+  SOURCE_SUBDIR llvm
+  PATCH_COMMAND sed -i.bak -e "/\\#include \\\"llvm\\/Support\\/Signals.h\\\"/i #include <stdint.h>" llvm/lib/Support/Signals.cpp &&
+    sed -i.bak -e "/\\#include <vector>/i #include <limits>" llvm/utils/benchmark/src/benchmark_register.h
+)
+
+ExternalProject_Add(
+  pluto
+  GIT_REPOSITORY https://github.com/kumasento/pluto
+  GIT_TAG 5603283fb3e74fb33c380bb52874972b440d51a2
+  PREFIX pluto
+  # SOURCE_DIR ${PLUTO_SOURCE_DIR}
+  # CONFIGURE_COMMAND ${CMAKE_COMMAND} -E env bash ${PLUTO_CONFIGURE_COMMAND}
+  CONFIGURE_COMMAND ./autogen.sh &&
+    ./configure --prefix=${CMAKE_CURRENT_BINARY_DIR}/pluto/install --with-clang-prefix=${PLUTO_LLVM_PREFIX}/llvm10-install
+  INSTALL_DIR ${CMAKE_CURRENT_BINARY_DIR}/pluto
+  BUILD_COMMAND make -j LDFLAGS="-Wl,--copy-dt-needed-entries"
   INSTALL_COMMAND make install
   BUILD_IN_SOURCE 1
-  BUILD_BYPRODUCTS 
+  BUILD_BYPRODUCTS
    "${PLUTO_LIB_DIR}/libpluto.so"
    "${PLUTO_LIB_DIR}/libisl.so"
    "${PLUTO_LIB_DIR}/libosl.so"
@@ -94,6 +57,8 @@ ExternalProject_Add(
    "${PLUTO_LIB_DIR}/libpolylib64.so"
    "${PLUTO_LIB_DIR}/libcandl.so"
 )
+
+add_dependencies(pluto llvm10)
 
 add_library(libpluto SHARED IMPORTED)
 set_target_properties(libpluto PROPERTIES IMPORTED_LOCATION "${PLUTO_LIB_DIR}/libpluto.so")
