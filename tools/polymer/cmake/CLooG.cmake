b6dcc20800153adedf1ca83ca50b7c17f3dd4abd
Emil VATAI <emil.vatai@gmail.com>
2023-09-15 12:16:18 +0900

    Make slow but steady progress

--- tools/polymer/cmake/CLooG.cmake
+++ tools/polymer/cmake/CLooG.cmake
@@ -4,17 +4,18 @@ include(ExternalProject)
 
 ExternalProject_Add(
   cloog
-  SOURCE_DIR "${CMAKE_SOURCE_DIR}/cloog"
-  CONFIGURE_COMMAND "${CMAKE_SOURCE_DIR}/cloog/autogen.sh" && "${CMAKE_SOURCE_DIR}/cloog/configure" --prefix=${CMAKE_BINARY_DIR}/cloog --with-osl-prefix="${CMAKE_BINARY_DIR}/openscop"
-  PREFIX ${CMAKE_BINARY_DIR}/cloog
+  GIT_REPOSITORY https://github.com/kumasento/cloog.git
+  GIT_TAG 43CFB85ED1E1BA1C2F27B450498522B35467ACE7
+  CONFIGURE_COMMAND "./autogen.sh" && "./configure" --prefix=${CMAKE_CURRENT_BINARY_DIR}/cloog --with-osl-prefix="${CMAKE_CURRENT_BINARY_DIR}/openscop"
+  PREFIX ${CMAKE_CURRENT_BINARY_DIR}/cloog
   BUILD_COMMAND make
   INSTALL_COMMAND make install
   BUILD_IN_SOURCE 1
-  BUILD_BYPRODUCTS "${CMAKE_BINARY_DIR}/cloog/lib/libcloog-isl.a" "${CMAKE_BINARY_DIR}/cloog/lib/libisl.a"
+  BUILD_BYPRODUCTS "${CMAKE_CURRENT_BINARY_DIR}/cloog/lib/libcloog-isl.a" "${CMAKE_CURRENT_BINARY_DIR}/cloog/lib/libisl.a"
 )
 
-set(CLOOG_INCLUDE_DIR "${CMAKE_BINARY_DIR}/cloog/include")
-set(CLOOG_LIB_DIR "${CMAKE_BINARY_DIR}/cloog/lib")
+set(CLOOG_INCLUDE_DIR "${CMAKE_CURRENT_BINARY_DIR}/cloog/include")
+set(CLOOG_LIB_DIR "${CMAKE_CURRENT_BINARY_DIR}/cloog/lib")
 
 add_library(libcloog SHARED IMPORTED)
 set_target_properties(libcloog PROPERTIES IMPORTED_LOCATION "${CLOOG_LIB_DIR}/libcloog-isl.a")
--- tools/polymer/cmake/OpenScop.cmake
+++ tools/polymer/cmake/OpenScop.cmake
@@ -3,18 +3,19 @@
 include(ExternalProject)
 
 ExternalProject_Add(
-  osl 
-  SOURCE_DIR "${CMAKE_SOURCE_DIR}/openscop"
-  CONFIGURE_COMMAND "${CMAKE_SOURCE_DIR}/openscop/autogen.sh" && "${CMAKE_SOURCE_DIR}/openscop/configure" --prefix=${CMAKE_BINARY_DIR}/openscop
-  PREFIX ${CMAKE_BINARY_DIR}/openscop
+  osl
+  GIT_REPOSITORY https://github.com/periscop/openscop.git
+  GIT_TAG 37805d8fef38c2d1b8aa8f5c26b40f79100322e7
+  CONFIGURE_COMMAND "./autogen.sh" && "./configure" --prefix=${CMAKE_CURRENT_BINARY_DIR}/openscop
+  PREFIX ${CMAKE_CURRENT_BINARY_DIR}/openscop
   BUILD_COMMAND make
   INSTALL_COMMAND make install
   BUILD_IN_SOURCE 1
-  BUILD_BYPRODUCTS "${CMAKE_BINARY_DIR}/openscop/lib/libosl.a"
+  BUILD_BYPRODUCTS "${CMAKE_CURRENT_BINARY_DIR}/openscop/lib/libosl.a"
 )
 
-set(OSL_INCLUDE_DIR "${CMAKE_BINARY_DIR}/openscop/include")
-set(OSL_LIB_DIR "${CMAKE_BINARY_DIR}/openscop/lib")
+set(OSL_INCLUDE_DIR "${CMAKE_CURRENT_BINARY_DIR}/openscop/include")
+set(OSL_LIB_DIR "${CMAKE_CURRENT_BINARY_DIR}/openscop/lib")
 
 add_library(libosl SHARED IMPORTED)
 set_target_properties(libosl PROPERTIES IMPORTED_LOCATION "${OSL_LIB_DIR}/libosl.a")
