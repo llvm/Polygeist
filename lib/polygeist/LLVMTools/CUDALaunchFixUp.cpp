2b6e53d3f5d610a8974d79b1040fd0653e69c4c2
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2023-10-14 02:24:28 +0900

    wip

--- lib/polygeist/LLVMTools/CUDALaunchFixUp.cpp
+++ lib/polygeist/LLVMTools/CUDALaunchFixUp.cpp
@@ -20,58 +20,62 @@ void fixup(Module &M) {
   if (!LaunchKernelFunc)
     return;
 
+  SmallVector<CallInst *> ToHandle;
   for (auto User : LaunchKernelFunc->users()) {
     if (auto CI = dyn_cast<CallInst>(User)) {
-      SmallVector<Type *> ArgTypes = {
-          // function ptr
-          PointerType::get(M.getContext(), 0),
-          // grid size
-          Type::getInt32Ty(M.getContext()),
-          Type::getInt32Ty(M.getContext()),
-          Type::getInt32Ty(M.getContext()),
-          // block siz3
-          Type::getInt32Ty(M.getContext()),
-          Type::getInt32Ty(M.getContext()),
-          Type::getInt32Ty(M.getContext()),
-          // dyn shmem size
-          Type::getInt32Ty(M.getContext()),
-          // stream
-          PointerType::get(M.getContext(), 0),
-      };
-      auto StubFunc = cast<Function>(CI->getArgOperand(0));
-      for (auto ArgTy : StubFunc->getFunctionType()->params())
-        ArgTypes.push_back(ArgTy);
-      auto PolygeistLaunchFunc = Function::Create(
-          FunctionType::get(Type::getVoidTy(M.getContext()), ArgTypes,
-                            /*isVarAtg=*/false),
-          llvm::GlobalValue::InternalLinkage,
-          "__polygeist_launch_kernel_" + StubFunc->getName(), M);
-
-      IRBuilder<> Builder(CI);
-      auto FuncPtr = CI->getArgOperand(0);
-      auto GridDim1 = CI->getArgOperand(1);
-      auto GridDim2 = CI->getArgOperand(2);
-      auto GridDimX = Builder.CreateTrunc(GridDim1, Builder.getInt32Ty());
-      auto GridDimY = Builder.CreateLShr(
-          GridDim1, ConstantInt::get(Builder.getInt32Ty(), 32));
-      GridDimY = Builder.CreateTrunc(GridDim1, Builder.getInt32Ty());
-      auto GridDimZ = GridDim2;
-      auto BlockDim1 = CI->getArgOperand(3);
-      auto BlockDim2 = CI->getArgOperand(4);
-      auto BlockDimX = Builder.CreateTrunc(BlockDim1, Builder.getInt32Ty());
-      auto BlockDimY = Builder.CreateLShr(
-          BlockDim1, ConstantInt::get(Builder.getInt32Ty(), 32));
-      BlockDimY = Builder.CreateTrunc(BlockDim1, Builder.getInt32Ty());
-      auto BlockDimZ = BlockDim2;
-      auto SharedMemSize = CI->getArgOperand(6);
-      auto StreamPtr = CI->getArgOperand(7);
-      SmallVector<Value *> Args = {
-          FuncPtr,   GridDimX,  GridDimY,      GridDimZ,  BlockDimX,
-          BlockDimY, BlockDimZ, SharedMemSize, StreamPtr,
-      };
-      Builder.CreateCall(PolygeistLaunchFunc, Args);
-      CI->eraseFromParent();
+      ToHandle.push_back(CI);
     }
+  }
+  for (CallInst *CI : ToHandle) {
+    SmallVector<Type *> ArgTypes = {
+        // function ptr
+        PointerType::get(M.getContext(), 0),
+        // grid size
+        Type::getInt32Ty(M.getContext()),
+        Type::getInt32Ty(M.getContext()),
+        Type::getInt32Ty(M.getContext()),
+        // block siz3
+        Type::getInt32Ty(M.getContext()),
+        Type::getInt32Ty(M.getContext()),
+        Type::getInt32Ty(M.getContext()),
+        // dyn shmem size
+        Type::getInt32Ty(M.getContext()),
+        // stream
+        PointerType::get(M.getContext(), 0),
+    };
+    auto StubFunc = cast<Function>(CI->getArgOperand(0));
+    for (auto ArgTy : StubFunc->getFunctionType()->params())
+      ArgTypes.push_back(ArgTy);
+    auto PolygeistLaunchFunc = Function::Create(
+        FunctionType::get(Type::getVoidTy(M.getContext()), ArgTypes,
+                          /*isVarAtg=*/false),
+        llvm::GlobalValue::InternalLinkage,
+        "__polygeist_launch_kernel_" + StubFunc->getName(), M);
+
+    IRBuilder<> Builder(CI);
+    auto FuncPtr = CI->getArgOperand(0);
+    auto GridDim1 = CI->getArgOperand(1);
+    auto GridDim2 = CI->getArgOperand(2);
+    auto GridDimX = Builder.CreateTrunc(GridDim1, Builder.getInt32Ty());
+    auto GridDimY = Builder.CreateLShr(
+        GridDim1, ConstantInt::get(Builder.getInt32Ty(), 32));
+    GridDimY = Builder.CreateTrunc(GridDim1, Builder.getInt32Ty());
+    auto GridDimZ = GridDim2;
+    auto BlockDim1 = CI->getArgOperand(3);
+    auto BlockDim2 = CI->getArgOperand(4);
+    auto BlockDimX = Builder.CreateTrunc(BlockDim1, Builder.getInt32Ty());
+    auto BlockDimY = Builder.CreateLShr(
+        BlockDim1, ConstantInt::get(Builder.getInt32Ty(), 32));
+    BlockDimY = Builder.CreateTrunc(BlockDim1, Builder.getInt32Ty());
+    auto BlockDimZ = BlockDim2;
+    auto SharedMemSize = CI->getArgOperand(6);
+    auto StreamPtr = CI->getArgOperand(7);
+    SmallVector<Value *> Args = {
+        FuncPtr,   GridDimX,  GridDimY,      GridDimZ,  BlockDimX,
+        BlockDimY, BlockDimZ, SharedMemSize, StreamPtr,
+    };
+    Builder.CreateCall(PolygeistLaunchFunc, Args);
+    CI->eraseFromParent();
   }
 }
 
