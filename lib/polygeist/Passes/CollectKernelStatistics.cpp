d587bbdf370ba419ba5f46a28645f6c61d8ce97a
Ivan Radanov Ivanov <ivanov.i.aa@m.titech.ac.jp>
2024-06-14 13:14:42 +0900

    .

--- lib/polygeist/Passes/CollectKernelStatistics.cpp
+++ lib/polygeist/Passes/CollectKernelStatistics.cpp
@@ -9,11 +9,11 @@
 #include "mlir/IR/Value.h"
 #include "mlir/IR/Verifier.h"
 #include "mlir/Pass/PassManager.h"
-#include "mlir/Support/MathExtras.h"
 #include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 #include "mlir/Transforms/Passes.h"
 
 #include "polygeist/Passes/Passes.h"
+#include "llvm/ADT/DynamicAPInt.h"
 
 using namespace mlir;
 using namespace polygeist;
@@ -36,7 +36,7 @@ static double estimateTripCount(Block *block, unsigned threadNum) {
           forOp.getUpperBound().getDefiningOp<arith::ConstantIndexOp>();
       auto stepCstOp = forOp.getStep().getDefiningOp<arith::ConstantIndexOp>();
       if (lbCstOp && ubCstOp && stepCstOp)
-        return mlir::ceilDiv(ubCstOp.value() - lbCstOp.value(),
+        return llvm::ceilDiv(ubCstOp.value() - lbCstOp.value(),
                              stepCstOp.value());
       else
         return 1.0;
@@ -203,8 +203,7 @@ std::array<StrideTy, 3> estimateStride(mlir::OperandRange indices,
         return 0;
       } else if (auto forOp =
                      dyn_cast<scf::ForOp>(ba.getOwner()->getParentOp())) {
-        return getTidXCoef(forOp.getOpOperandForRegionIterArg(ba).get(),
-                           isTidI);
+        return getTidXCoef(forOp.getTiedLoopInit(ba)->get(), isTidI);
       } else {
         return UNKNOWN;
       }
